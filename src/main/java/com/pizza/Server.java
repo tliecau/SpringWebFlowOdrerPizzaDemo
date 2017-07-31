0d0179957996fedc848f7a0507379c6bfbbf07e9
wallie <tliecau@gmail.com>
2017-08-01 01:25:16 +0200

    Gitignore update

--- src/main/java/com/pizza/Server.java
+++ src/main/java/com/pizza/Server.java
@@ -1,7 +1,15 @@
 package com.pizza;
 
-/**
- * Created by walli on 31.07.2017.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Server {
+    public static void main(String[] args) {
+        // Tell server to look for accounts-server.properties or
+        // accounts-server.yml
+        System.setProperty("spring.config.name", "accounts-server");
+
+        SpringApplication.run(Server.class, args);
+    }
 }
--- src/main/java/com/pizza/config/JspConfig.java
+++ src/main/java/com/pizza/config/JspConfig.java
@@ -1,17 +1,13 @@
 package com.pizza.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
-import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @Configuration
 @EnableWebMvc
